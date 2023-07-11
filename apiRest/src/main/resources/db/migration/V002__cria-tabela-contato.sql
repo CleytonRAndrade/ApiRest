d6513b01eeb922b7e7a57e2f67bbc463cf53a2ba
Cleyton Rafael <andrade200890@gmail.com>
2023-07-11 09:08:52 -0300

    Update V002__cria-tabela-contato.sql
    Indentação para melhor visualização

--- apiRest/src/main/resources/db/migration/V002__cria-tabela-contato.sql
+++ apiRest/src/main/resources/db/migration/V002__cria-tabela-contato.sql
@@ -1,14 +1,11 @@
 create table contato (
 
-	id        bigint       not null auto_increment,
+    id        bigint       not null auto_increment,
     pessoa_id bigint       not null,
-	nome      varchar(60)  not null,
-	telefone  varchar(20)  not null,
+    nome      varchar(60)  not null,
+    telefone  varchar(20)  not null,
     email     varchar(255) not null,
-	
-	
-	primary  key (id)
+    primary  key (id)
 );
-
 alter table contato add constraint fk_contato_pessoa
 foreign key (pessoa_id) references pessoa (id);
